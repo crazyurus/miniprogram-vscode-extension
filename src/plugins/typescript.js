890e675462e1e9b1b81f8d3e7648c4af3ec26833
Crazy Urus <crazyurus@vip.qq.com>
2022-04-24 13:53:15 +0800

    feat: 通过 no-save 安装类型文件

--- src/plugins/typescript.js
+++ src/plugins/typescript.js
@@ -1,30 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const vscode = require('vscode');
-const { readJSON } = require('../utils/json');
 const { getCurrentFolderPath } = require('../utils/path');
 
 function activate() {
   const rootPath = getCurrentFolderPath();
-  const packageJSONPath = path.join(rootPath, 'package.json');
-
-  if (!fs.existsSync(packageJSONPath)) {
-    return;
-  }
-
-  const content = readJSON(packageJSONPath);
+  const typeFilePath = path.join(rootPath, 'node_modules', '@types', 'wechat-miniprogram');
   const library = '@types/wechat-miniprogram';
 
-  if (
-    (content.dependencies && content.dependencies.hasOwnProperty(library)) ||
-    (content.devDependencies && content.devDependencies.hasOwnProperty(library))
-  ) {
+  if (fs.existsSync(typeFilePath)) {
     return;
   }
 
   vscode.tasks.executeTask(new vscode.Task({
     type: 'miniprogram.tasks.install-types',
-  }, vscode.ConfigurationTarget.WorkspaceFolder, '下载小程序 API 描述文件', 'npm', new vscode.ShellExecution('npm', ['install', '-D', library, '--registry=https://registry.npmmirror.com'], {
+  }, vscode.ConfigurationTarget.WorkspaceFolder, '下载小程序 API 描述文件', 'npm', new vscode.ShellExecution('npm', ['install', '--no-save', library, '--registry=https://registry.npmmirror.com'], {
     cwd: rootPath,
   })));
 }
