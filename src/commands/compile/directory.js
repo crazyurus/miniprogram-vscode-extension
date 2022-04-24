9be0f372690212b37d05140b954d0af2dc0c4e31
Crazy Urus <crazyurus@vip.qq.com>
2022-04-24 15:57:36 +0800

    fix: 设置小程序 miniprogramRoot 未使用相对路径

--- src/commands/compile/directory.js
+++ src/commands/compile/directory.js
@@ -1,4 +1,5 @@
 const vscode = require('vscode');
+const path = require('path');
 const fs = require('fs');
 const { updateJSON } = require('../../utils/json');
 const { getCurrentFolderPath, getProjectConfigPath } = require('../../utils/path');
@@ -9,7 +10,7 @@ function compileDirectory() {
     const projectFilePath = getProjectConfigPath(rootPath);
 
     if (fs.existsSync(projectFilePath)) {
-      updateJSON(projectFilePath, 'miniprogramRoot', e.fsPath);
+      updateJSON(projectFilePath, 'miniprogramRoot', path.relative(rootPath, e.fsPath));
       vscode.window.showInformationMessage('设置成功，当预览或上传小程序时，将仅打包此目录下的文件');
     } else {
       vscode.window.showErrorMessage('未找到 project.config.json 文件');
