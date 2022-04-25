65a8c1d844584d2f2b2c441b2a08fe351e9dbae2
Crazy Urus <crazyurus@vip.qq.com>
2022-04-25 10:17:47 +0800

    chore: 优化 dev 脚本

--- src/dev.js
+++ src/dev.js
@@ -1,32 +1,5 @@
 require('ts-node').register();
 
-const CommandPlugin = require('./commands');
-const { default: ViewPlugin } = require('./plugins/view');
-const { default: ExtensionPlugin } = require('./plugins/extension');
-const { default: TypeScriptPlugin } = require('./plugins/typescript');
-const { default: ProxyPlugin } = require('./plugins/proxy');
+const extension = require('./index');
 
-const plugins = [
-  ViewPlugin,
-  CommandPlugin.create,
-  CommandPlugin.compile,
-  CommandPlugin.project,
-  CommandPlugin.document,
-  CommandPlugin.storage,
-  ExtensionPlugin,
-  TypeScriptPlugin,
-  ProxyPlugin,
-];
-
-function activate(context) {
-  plugins.forEach(plugin => plugin.activate(context));
-}
-
-function deactivate() {
-  plugins.forEach(plugin => plugin.deactivate());
-}
-
-module.exports = {
-  activate,
-  deactivate,
-};
+module.exports = extension;
