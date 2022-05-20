3244710b166fc2cdb0d42a32587802f6965b1020
Crazy Urus <crazyurus@vip.qq.com>
2022-05-20 14:28:38 +0800

    refactor: 使用 undici 替代请求

--- src/commands/document.ts
+++ src/commands/document.ts
@@ -1,35 +1,37 @@
 import * as vscode from 'vscode';
 import * as http from 'http';
-import * as https from 'https';
+import { fetch } from 'undici';
 import { openURL } from '../utils/ui';
+
+function createServer(url: string, title: string): void {
+  const baseURL = 'https://developers.weixin.qq.com';
+  const server = http.createServer(async (request, response) => {
+    const result = await fetch(baseURL + request.url);
+    let content = await result.text();
+
+    response.writeHead(result.status || 200, {});
+    content = content.replace('<body>', '<body style="background-color: #fff">')
+    response.end(content);
+  });
+  server.listen();
+
+  server.on('listening', async () => {
+    const address = server.address() as { port: number };
+
+    if (address) {
+      const webviewURL = url.replace(baseURL, `http://localhost:${address.port}`);
+      const webview = await openURL(webviewURL, title);
+
+      webview.onDidDispose(() => {
+        server.close();
+      });
+    }
+  });
+}
 
 function activate(): void {
   vscode.commands.registerCommand('MiniProgram.commands.document.open', () => {
-    const server = http.createServer((request, response) => {
-      https.get('https://developers.weixin.qq.com' + request.url, result => {
-        let body = '';
-
-        result.on('data', chunk => body += chunk);
-        result.on('end', () => {
-          response.writeHead(result.statusCode || 200, {});
-          body = body.replace('<body>', '<body style="background-color: #fff">')
-          response.end(body);
-        });
-      });
-    });
-    server.listen();
-
-    server.on('listening', async () => {
-      const address = server.address() as { port: number };
-
-      if (address) {
-        const webview = await openURL(`http://localhost:${address.port}/miniprogram/dev/framework/`, '微信官方文档');
-
-        webview.onDidDispose(() => {
-          server.close();
-        });
-      }
-    });
+    createServer('https://developers.weixin.qq.com/miniprogram/dev/framework/', '微信开发文档');
   });
 
   vscode.commands.registerCommand('MiniProgram.commands.document.search', () => {
