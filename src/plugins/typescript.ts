82aac8c4fb8e61ab128f53f68bd23d4f92c50bbf
Crazy Urus <crazyurus@vip.qq.com>
2022-12-03 02:55:06 +0800

    refactor: 优化 TypeScript 支持的实现

--- src/plugins/typescript.ts
+++ src/plugins/typescript.ts
@@ -1,24 +1,23 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as vscode from 'vscode';
 import { getCurrentFolderPath } from '../utils/path';
 import Plugin from '../base';
 
 class TypeScriptPlugin extends Plugin {
   activate(): void {
     const rootPath = getCurrentFolderPath();
-    const typeFilePath = path.join(rootPath, 'node_modules', '@types', 'wechat-miniprogram');
-    const library = '@types/wechat-miniprogram';
+    const jsConfigFilePath = path.join(rootPath, 'jsconfig.json');
+    const jsConfig = {
+      typeAcquisition: {
+        include: ['wechat-miniprogram'],
+      },
+    };
 
-    if (fs.existsSync(typeFilePath)) {
+    if (fs.existsSync(jsConfigFilePath)) {
       return;
     }
 
-    vscode.tasks.executeTask(new vscode.Task({
-      type: 'miniprogram.tasks.install-types',
-    }, '下载小程序 API 描述文件', 'npm', new vscode.ShellExecution('npm', ['install', '--no-save', library, '--registry=https://registry.npmmirror.com'], {
-      cwd: rootPath,
-    })));
+    fs.promises.writeFile(jsConfigFilePath, JSON.stringify(jsConfig, null, 2));
   }
 }
 
