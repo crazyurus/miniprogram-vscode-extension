d2a7d5ef5228bda80ef84e44b245b84b4c93668e
Crazy Urus <crazyurus@vip.qq.com>
2024-08-16 22:38:39 +0800

    refactor: 移除 asar

--- src/entry/development.js
+++ src/entry/development.js
@@ -1,4 +1,9 @@
+/* eslint-disable n/no-unpublished-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable n/no-missing-require */
 require('ts-node').register();
+
+process.env.APPINSIGHTS_INSTRUMENTATIONKEY = '123456';
 
 const extension = require('../index');
 
--- src/entry/production.js
+++ src/entry/production.js
@@ -1,26 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const Module = require('node:module');
-const path = require('node:path');
-const asarPath = path.join(__dirname, '..', 'node_modules.asar');
-const nodeModulesPath = path.join(__dirname, '..', '..', 'node_modules');
-const originalResolveLookupPaths = Module._resolveLookupPaths;
-
-Module._resolveLookupPaths = function (moduleName, parent) {
-  const paths = originalResolveLookupPaths(moduleName, parent);
-
-  if (Array.isArray(paths)) {
-    for (let i = 0, len = paths.length; i < len; i++) {
-      if (paths[i] === nodeModulesPath) {
-        paths.splice(i, 0, asarPath);
-        break;
-      }
-    }
-  }
-
-  return paths;
-};
-
-// eslint-disable-next-line n/no-missing-require
+// eslint-disable-next-line n/no-missing-require, @typescript-eslint/no-var-requires
 const extension = require('./index');
 
 module.exports = extension;
